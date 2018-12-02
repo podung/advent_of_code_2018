122ca8843eccb337db69eee659572392174b05b4
Joe DePung <joe.depung@gmail.com>
2018-12-01 22:22:13 -0600

    Day 1, problem 2

--- day_01.exs
+++ day_01.exs
@@ -1,10 +1,46 @@
 defmodule Exercise1 do
-  def calibrate(frequencies) when is_binary frequencies do
+  defmodule Accumulator do
+    defstruct current: 0, seen: MapSet.new([0]), calibrated: false
+  end
+
+  def calibrate_simple(frequencies) when is_binary frequencies do
     frequencies
     |> String.split("\n", trim: true)
     |> Enum.map(&Integer.parse/1)
     |> Enum.map(&(elem(&1, 0)))
     |> Enum.sum
+  end
+
+  def calibrate(frequencies) when is_binary frequencies do
+    frequencies
+    |> String.split("\n", trim: true)
+    |> Enum.map(&Integer.parse/1)
+    |> Enum.map(&(elem(&1, 0)))
+    |> do_calibrate
+  end
+
+  defp do_calibrate(_, acc \\ %Accumulator{})
+
+  defp do_calibrate(_, acc = %Accumulator{ calibrated: true }) do
+    acc.current
+  end
+
+  defp do_calibrate(list = [_|_], acc) do
+    result = Enum.reduce_while(list, acc, &process/2)
+
+    do_calibrate(list, result)
+  end
+
+  defp process(adjustment, %Accumulator{} = acc) do
+    current = adjustment + acc.current
+
+    if (MapSet.member?(acc.seen, current)) do
+      { :halt, %{ acc | current: current, calibrated: true } }
+
+    else
+      { :cont, %{ acc | current: current,
+                        seen: MapSet.put(acc.seen, current) } }
+    end
   end
 end
 
@@ -15,29 +51,70 @@ defmodule Exercise1Test do
 
   import Exercise1
 
-  test "calibrate test data" do
-    assert calibrate("""
-                     +1
-                     +1
-                     +1
-                     """) == 3
+  describe "calibrate_simple" do
+    test "calibrate_simple scenarios" do
+      assert calibrate_simple("""
+                              +1
+                              +1
+                              +1
+                              """) == 3
 
-    assert calibrate("""
-                     +1
-                     +1
-                     -2
-                     """) == 0
+      assert calibrate_simple("""
+                              +1
+                              +1
+                              -2
+                              """) == 0
 
-    assert calibrate("""
-                     -1
-                     -2
-                     -3
-                     """) == -6
+      assert calibrate_simple("""
+                              -1
+                              -2
+                              -3
+                              """) == -6
+    end
+
+    test "frequencies given for problem" do
+      frequencies = File.read! "fixtures/day_01_frequencies.txt"
+
+      assert calibrate_simple(frequencies) == 490
+    end
   end
 
-  test "frequencies given for problem" do
-		frequencies = File.read! "fixtures/day_01_frequencies.txt"
+  describe "calibrate" do
+    test "calibrate scenarios" do
+      assert calibrate("""
+                       +1
+                       -1
+                       """) == 0
 
-		assert calibrate(frequencies) == 490
-	end
+      assert calibrate("""
+                       +3
+                       +3
+                       +4
+                       -2
+                       -4
+                       """) == 10
+
+      assert calibrate("""
+                       -6
+                       +3
+                       +8
+                       +5
+                       -6
+                       """) == 5
+
+      assert calibrate("""
+                       +7
+                       +7
+                       -2
+                       -7
+                       -4
+                       """) == 14
+    end
+
+    test "frequencies given for problem" do
+      frequencies = File.read! "fixtures/day_01_frequencies.txt"
+
+      assert calibrate(frequencies) == 70357
+    end
+  end
 end
