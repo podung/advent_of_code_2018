9efcb5b99b06738e882f857d9f6779c0f6b7f7e9
Joe DePung <joe.depung@gmail.com>
2018-12-12 10:38:31 -0600

    Day 5 part 2

--- day_05.exs
+++ day_05.exs
@@ -9,6 +9,38 @@ defmodule Day5 do
     |> Enum.join
   end
 
+  def trigger_part2(polymer) do
+    polymer
+    |> String.trim
+    |> String.codepoints
+    |> Enum.reduce({ [], %{} }, &react_test/2)
+    |> elem(1)
+    |> Enum.min_by(&polymer_length/1)
+    |> elem(1)
+    |> Enum.reverse
+    |> Enum.join
+  end
+
+  defp react_test(current, { full, candidates } ) do
+    candidates = candidates
+                 |> Map.put_new(String.upcase(current), full)
+                 |> Enum.map(&(map_it(current, &1)))
+                 |> Enum.into(%{})
+
+
+    { react(current, full), candidates }
+  end
+
+  defp map_it(current, { key, value }) do
+    if String.upcase(current) == key do
+      { key, value }
+    else
+      { key, react(current, value) }
+    end
+  end
+
+  defp polymer_length({ _key, polymer }), do: length(polymer)
+
   defp react(current, []), do: [current]
 
   defp react(current, [last | tail]) do
@@ -48,6 +80,22 @@ defmodule Day5Test do
       polymer = File.read! "fixtures/day_05_polymer.txt"
 
       assert trigger(polymer) |> String.length == 9060
+    end
+  end
+
+  describe "trigger_part2" do
+    test "examples" do
+      assert trigger_part2("dabAcCaCBAcCcaDA") == "daDA"
+    end
+
+    test "examples length" do
+      assert trigger_part2("dabAcCaCBAcCcaDA") |> String.length == 4
+    end
+
+    test "length answer" do
+      polymer = File.read! "fixtures/day_05_polymer.txt"
+
+      assert trigger_part2(polymer) |> String.length == 6310
     end
   end
 end
